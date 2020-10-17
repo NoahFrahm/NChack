6d884ba063c1778004489f995c0e4e8cf4b9450e
Noah Frahm <nofrahm@live.unc.edu>
2020-10-17 13:35:36 -0400

    first commit

---
+++ webscrape.py/test.py
@@ -0,0 +1,39 @@
+from urllib.parse import urlsplit
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from typing import List, Dict
+
+
+class CourseSpider(scrapy.Spider):
+    name: str = "coursespider"
+
+
+    def start_request(self):
+        """This will scrape the list of Urls given."""
+        urls: List[str] = ['https://catalog.unc.edu/courses/']
+        for url in urls:
+            yield scrapy.Request( url = url, callback = self.parse)
+        
+
+    def parse(self, response):
+        """This will scrape the course catalog to get to each department."""
+        links = response.css('/html/ul[1]//li/@href').extract()
+        for link in links:
+            course_dictionary['test'] = link
+            # yield response.follow( url = link, callback = self.parse2)
+
+
+    # def parse2(self, response):
+    #     """This will get all the course names/numbers from each department."""
+    #     department_name: str = response.xpath()
+    #     course_names: str = response.xpath()
+    #     course_dictionary[department_name] = course_names
+
+course_dictionary: Dict[str,List[str]] = {}
+
+process = CrawlerProcess()
+process.crawl(CourseSpider)
+process.start()
+
+
+
