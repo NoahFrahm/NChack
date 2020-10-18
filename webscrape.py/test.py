adef29a95825a5a814d22e371b3b8fc489f2567b
Noah Frahm <nofrahm@live.unc.edu>
2020-10-18 01:12:39 -0400

    csv_files containing course number and description

--- webscrape.py/test.py
+++ webscrape.py/test.py
@@ -4,16 +4,21 @@ from bs4 import BeautifulSoup
 from typing import List, Dict
 import csv
 import itertools
+import re
 
-
+# make to identical dict but in place of course number put course description
 def main() -> None:
+    """Main function of the web crawler."""
     urls: List[str] = set_up()
-    data: Dict[str,List[str]] = crawl(urls)
-    csv_builder(data)
+    data, datadesc = crawl(urls)
+    csv_builder(data, 'data113.csv')
+    csv_builder(datadesc, 'data112.csv')
+    return "done"
 
 
-def csv_builder(data: Dict[str,List[str]]) -> None:
-    with open("data111.csv", "w") as outfile:
+def csv_builder(data: Dict[str,List[str]],file_name: str) -> None:
+    """Builds data into a csv file."""
+    with open(file_name, "w") as outfile:
         writer = csv.writer(outfile)
         writer.writerow(data.keys())
         writer.writerows(itertools.zip_longest(*data.values()))
@@ -33,6 +38,7 @@ def filtered(urls: List[str]) -> List[str]:
 
 
 def set_up() -> List[str]:
+    """Pulls all the links to department courses from catalog."""
     URL = 'https://catalog.unc.edu/courses/'
     page = requests.get(URL)
 
@@ -50,16 +56,22 @@ def set_up() -> List[str]:
 
 
 def crawl(links: List[str]) -> Dict[str,List[str]]:
+    """Goes through the gathered links and extracts course data."""
     courses: Dict[str,List[int]] = {}
+    course_descrp: Dict[str,List[int]] = {}
 
     for link in links: # in range(0,4):
-        data: Dict[str,List[str]] = department_courses(link)
+        data: Dict[str, List[str]] = department_courses(link)
+        datadesc: Dict[str, List[str]] = course_filtering(link)
         for key in data:
             courses[key] = data[key]
-    return courses
+            course_descrp[key] = datadesc[key]
+            
+    return courses, course_descrp
 
 
 def department_courses(link: str) -> Dict[str, List[int]]:
+    """Makes a dictionary of all course numbers in respective department."""
     courses: List[str] = []
     page = requests.get(link)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -92,6 +104,56 @@ def department_courses(link: str) -> Dict[str, List[int]]:
     # implement search by abreviation vs department, two dicts
     return {abreviation: courses}
 
+
+def course_description(link: str) -> Dict[str,List[str]]:
+    courses_descr: List[str] = []
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    department = soup.select('h1.page-title')[0].get_text()
+
+    # blocks = soup.select('p.courseblockdesc') #changed here
+    blocks = soup.select("p.courseblockdesc")
+    
+    for course in blocks:
+        cdesc: str = course.get_text()
+        cdesc[5:20]
+        courses_descr.append(cdesc)
+
+    start: int = len(department) - 5
+    stop: int = len(department) - 1
+    abreviation: str = department[start:stop]
+    if '(' in abreviation:
+        abreviation = abreviation[1:4]
+
+    return {abreviation: courses_descr}
+
+
+def course_filtering(link: str) -> Dict[str,str]:
+    soy = course_description(link)
+    for key in soy:
+        new_list: List[str] = []
+        for i in range(0,len(soy[key])):
+            rum = soy[key][i]
+            try:
+                a = re.search('.Ge', rum)
+                s = a.start()
+                new_rum = rum[1:s]
+                new_list.append(new_rum)    
+            except:
+                try:
+                    a = re.search('.Re', rum)
+                    s = a.start()
+                    new_rum = rum[1:s]
+                    new_list.append(new_rum)
+                except:
+                    try:
+                        a = re.search('.Re', rum)
+                        s = a.start()
+                        new_rum = rum[1:s]
+                        new_list.append(new_rum)
+                    except:
+                        ...
+        return {key:new_list}
 
 # courses: List[str] = []
 # page = requests.get('https://catalog.unc.edu/courses/bcb/')
--- webscrape.py/textbook_webscrape.py
+++ webscrape.py/textbook_webscrape.py
@@ -2,10 +2,9 @@ from typing import List,Dict
 import csv
 import itertools
 
+import re
 
-d = {"key1": [1,2,3,4], "key2": [4,5,6], "key3": [7,8,9]}
-with open("tester.csv", "w") as outfile:
-   writer = csv.writer(outfile)
-   writer.writerow(d.keys())
-   writer.writerows(itertools.zip_longest(*d.values()))
+string = 'This is laughing laugh'
 
+a = re.search(r'\b(is)\b', string)
+print(a.start())
